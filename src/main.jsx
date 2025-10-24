10a6a2e1cc742e7d426a3e1cd733f7212d170aa6
Rahuljoshi98 <2000rahuljoshi@gmail.com>
2025-10-24 14:26:30 +0530

    redux setup completed

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import App from './App'
-import Login from './pages/Login'
-import Signup from './pages/Signup'
-import Dashboard from './pages/Dashboard'
-import { AuthProvider } from './store/AuthContext'
-import ProtectedRoute from './routes/ProtectedRoute'
-import PublicRoute from './routes/PublicRoute'
-import './index.css'
-import { Toaster } from 'react-hot-toast'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Dashboard from './pages/Dashboard';
+import { AuthProvider } from './store/AuthContext';
+import ProtectedRoute from './routes/ProtectedRoute';
+import PublicRoute from './routes/PublicRoute';
+import './index.css';
+import { Toaster } from 'react-hot-toast';
+import ReduxProvider from './store/redux/reduxProvider';
 
 const router = createBrowserRouter([
   {
@@ -20,24 +21,24 @@ const router = createBrowserRouter([
         element: <PublicRoute />,
         children: [
           { path: '/', element: <Login /> },
-          { path: '/signup', element: <Signup /> },
-        ],
+          { path: '/signup', element: <Signup /> }
+        ]
       },
       {
         element: <ProtectedRoute />,
-        children: [
-          { path: '/dashboard', element: <Dashboard /> },
-        ],
-      },
-    ],
-  },
-])
+        children: [{ path: '/dashboard', element: <Dashboard /> }]
+      }
+    ]
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProvider>
-       <Toaster position="top-right" reverseOrder={false} />
-      <RouterProvider router={router} />
-    </AuthProvider>
+    <ReduxProvider>
+      <AuthProvider>
+        <Toaster position='top-right' reverseOrder={false} />
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </ReduxProvider>
   </React.StrictMode>
-)
+);
