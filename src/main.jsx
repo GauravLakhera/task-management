ed6be7a3bd2044063f9b4e0e01f036ddea8abeab
Rahuljoshi98 <2000rahuljoshi@gmail.com>
2025-10-24 15:03:06 +0530

    redux setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
@@ -12,73 +11,39 @@ import PublicRoute from './routes/PublicRoute';
 import './index.css';
 import { Toaster } from 'react-hot-toast';
 import ReduxProvider from './store/redux/reduxProvider';
-=======
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from "./App";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import Dashboard from "./pages/Dashboard";
-import { AuthProvider } from "./store/AuthContext";
-import ProtectedRoute from "./routes/ProtectedRoute";
-import PublicRoute from "./routes/PublicRoute";
-import "./index.css";
-import { Toaster } from "react-hot-toast";
-import ProjectDetails from "./pages/ProjectDetails";
->>>>>>> 94206718a9956879fe817a4e725e2407dcd60ed8
+import ProjectDetails from './pages/ProjectDetails';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <App />,
     children: [
       {
         element: <PublicRoute />,
         children: [
-<<<<<<< HEAD
           { path: '/', element: <Login /> },
+          { path: '/login', element: <Login /> },
           { path: '/signup', element: <Signup /> }
         ]
       },
       {
         element: <ProtectedRoute />,
-        children: [{ path: '/dashboard', element: <Dashboard /> }]
+        children: [
+          { path: '/dashboard', element: <Dashboard /> },
+          { path: '/project/:projectId', element: <ProjectDetails /> }
+        ]
       }
     ]
   }
-=======
-          { path: "/", element: <Login /> },
-          { path: "/login", element: <Login /> },
-          { path: "/signup", element: <Signup /> },
-        ],
-      },
-      {
-        element: <ProtectedRoute />,
-        children: [
-          { path: "/dashboard", element: <Dashboard /> },
-          { path: "/project/:projectId", element: <ProjectDetails /> },
-        ],
-      },
-    ],
-  },
->>>>>>> 94206718a9956879fe817a4e725e2407dcd60ed8
 ]);
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-<<<<<<< HEAD
     <ReduxProvider>
       <AuthProvider>
         <Toaster position='top-right' reverseOrder={false} />
         <RouterProvider router={router} />
       </AuthProvider>
     </ReduxProvider>
-=======
-    <AuthProvider>
-      <Toaster position="top-right" reverseOrder={false} />
-      <RouterProvider router={router} />
-    </AuthProvider>
->>>>>>> 94206718a9956879fe817a4e725e2407dcd60ed8
   </React.StrictMode>
 );
